93c4e29d8cad88801e9e7650cd6abf4cdcb96e23
Dmitri <dmitriylobachevskiy@gmail.com>
2020-03-10 13:17:34 +0300

    feat: add self-esteem block with dynamic checkboxes

--- src/app/worklog/components/our-worklog/our-worklog.component.ts
+++ src/app/worklog/components/our-worklog/our-worklog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TeamMember, teamMembersWorklog } from '../../constants/team-members';
+import { extraScope, minScope, normalScope, ScopeItem } from '../../constants/scopes';
 
 @Component({
   selector: 'app-our-worklog',
@@ -9,7 +10,49 @@ import { TeamMember, teamMembersWorklog } from '../../constants/team-members';
 export class OurWorklogComponent implements OnInit {
   public displayedColumns: string[] = ['feature', 'time'];
   public team: TeamMember[] = teamMembersWorklog;
-  constructor() {}
+  public minScope: ScopeItem[] = minScope;
+  public normalScope: ScopeItem[] = normalScope;
+  public extraScope: ScopeItem[] = extraScope;
+  public minScopeTotal: number;
+  public minScopeActual: number;
+  public normalScopeTotal: number;
+  public normalScopeActual: number;
+  public extraScopeTotal: number;
+  public extraScopeActual: number;
+  public totalScore: number;
+  public actualScore: number;
+  constructor() {
+    this.minScopeTotal = this.minScope.reduce((acc, item) => acc + item.value, 0);
+    this.normalScopeTotal = this.normalScope.reduce((acc, item) => acc + item.value, 0);
+    this.extraScopeTotal = this.extraScope.reduce((acc, item) => acc + item.value, 0);
+    this.totalScore = this.minScopeTotal + this.normalScopeTotal + this.extraScopeTotal;
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.onMinScopeChange();
+    this.onNormalScopeChange();
+    this.onExtraScopeChange();
+  }
+
+  public onMinScopeChange(): void {
+    this.minScopeActual = this.minScope
+      .filter(item => item.completed)
+      .reduce((acc, item) => acc + item.value, 0);
+    this.countActualScore();
+  }
+  public onNormalScopeChange(): void {
+    this.normalScopeActual = this.normalScope
+      .filter(item => item.completed)
+      .reduce((acc, item) => acc + item.value, 0);
+    this.countActualScore();
+  }
+  public onExtraScopeChange(): void {
+    this.extraScopeActual = this.extraScope
+      .filter(item => item.completed)
+      .reduce((acc, item) => acc + item.value, 0);
+    this.countActualScore();
+  }
+  public countActualScore(): void {
+    this.actualScore = this.minScopeActual + this.normalScopeActual + this.extraScopeActual;
+  }
 }
--- src/app/worklog/worklog.module.ts
+++ src/app/worklog/worklog.module.ts
@@ -4,10 +4,11 @@ import { OurWorklogComponent } from './components/our-worklog/our-worklog.compon
 import { TranslateModule } from '@ngx-translate/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [OurWorklogComponent],
-  imports: [CommonModule, TranslateModule, MatExpansionModule, MatTableModule],
-  exports: [OurWorklogComponent, MatExpansionModule, MatTableModule]
+  imports: [CommonModule, TranslateModule, MatExpansionModule, MatTableModule, MatCheckboxModule],
+  exports: [OurWorklogComponent, MatExpansionModule, MatTableModule, MatCheckboxModule]
 })
 export class WorklogModule {}
