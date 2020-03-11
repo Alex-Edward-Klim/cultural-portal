98ee91e563902a08bdabf6bd8f999be583cc0fb4
vladimir-drachuk <vvdra4@gmail.com>
2020-03-11 15:29:17 +0300

    feat: add responsive header

--- src/app/core/components/content/content.component.ts
+++ src/app/core/components/content/content.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent implements OnInit {
+export class ContentComponent {
 
-  constructor() { }
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  @ViewChild('content') content: MatSidenavContent;
+  public navigator;
 
-  ngOnInit(): void {
+  constructor(private nav: NavigationService) {
+    this.navigator = this.nav.navMenu;
   }
 
+  public open(): void {
+    this.sidenav.open();
+    this.content.getElementRef().nativeElement.classList.add('content-blocked');    
+  }
+
+  public close(): void {
+    this.sidenav.close();
+    this.content.getElementRef().nativeElement.classList.remove('content-blocked');
+  } 
 }
