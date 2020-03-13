2d342e4644aabde5dc90b5af32a9ef866d988327
alex_klymenko <klymenko.sasha21@gmail.com>
2020-03-13 14:37:30 +0200

    feat: scroll to top after navigating throw the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { ContentComponent } from './core/components/content/content.component';
-import { IntroComponent } from './core/components/intro/intro.component';
-import { MainComponent } from './main/components/main/main.component';
-import { ListOfArchitectsComponent } from './architects/components/list-of-architects/list-of-architects.component';
-import { OurTeamComponent } from './team/components/our-team/our-team.component';
-import { OurWorklogComponent } from './worklog/components/our-worklog/our-worklog.component';
-import { OurStyleguideComponent } from './styleguide/components/our-styleguide/our-styleguide.component';
-import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
-import { ArchitectDetailedComponent } from './architects/components/architect-detailed/architect-detailed.component';
+import {ContentComponent} from './core/components/content/content.component';
+import {IntroComponent} from './core/components/intro/intro.component';
+import {MainComponent} from './main/components/main/main.component';
+import {ListOfArchitectsComponent} from './architects/components/list-of-architects/list-of-architects.component';
+import {OurTeamComponent} from './team/components/our-team/our-team.component';
+import {OurWorklogComponent} from './worklog/components/our-worklog/our-worklog.component';
+import {OurStyleguideComponent} from './styleguide/components/our-styleguide/our-styleguide.component';
+import {PageNotFoundComponent} from './core/components/page-not-found/page-not-found.component';
+import {ArchitectDetailedComponent} from './architects/components/architect-detailed/architect-detailed.component';
 
 const routes: Routes = [
   {
@@ -53,7 +53,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
